d9cba47f95b8693edaa2f44614b893ef2f29e4af
redagrd <reda.guerrouad@gmail.com>
2024-11-21 08:51:19 +0100

    Update unit test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,26 +1,17 @@
-# test_main.py
+from main import predict
+from pydantic import BaseModel
 
-from fastapi.testclient import TestClient
-from main import app  # Importez votre application FastAPI
-
-# Initialiser le client de test pour l'application FastAPI
-client = TestClient(app)
+class MockRequest(BaseModel):
+    client_id: int
 
 def test_predict_valid_client():
-    """Tester l'API avec un client existant pour vérifier la prédiction."""
-    response = client.post("/predict", json={"client_id": 100007})
-    assert response.status_code == 200
-    data = response.json()
-    assert "prediction" in data  # Vérifie si la réponse contient le champ 'prediction'
-    assert data["prediction"] in [0, 1]  # Vérifie si la prédiction est 0 ou 1
+    request = MockRequest(client_id=100007)
+    response = predict(request)
+    assert response["prediction"] in [0, 1]
 
-def test_predict_nonexistent_client():
-    """Tester l'API avec un client non-existant pour vérifier la gestion des erreurs."""
-    response = client.post("/predict", json={"client_id": 999999})
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Client ID not found"
-
-def test_predict_missing_client_id():
-    """Tester l'API avec des données incorrectes pour vérifier la validation d'entrée."""
-    response = client.post("/predict", json={})
-    assert response.status_code == 422  # Unprocessable Entity pour un identifiant manquant
+def test_predict_invalid_client():
+    request = MockRequest(client_id=999999)  # ID inexistant
+    try:
+        predict(request)
+    except Exception as e:
+        assert "Client ID not found" in str(e)
