faa3d773398a18371c440ca0bcf667deb5b054a1
redagrd <reda.guerrouad@gmail.com>
2024-11-21 11:01:25 +0100

    Update unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,15 +16,13 @@ def test_predict_valid_client():
 def test_predict_nonexistent_client():
     """Tester l'API avec un client non-existant pour vérifier la gestion des erreurs."""
     response = requests.post(f"{API_URL}/predict", json={"client_id": 999999})
+    
+    # Vérifiez si le statut est 404
+    assert response.status_code == 404
 
-    # Vérifiez si le statut est 404 ou 500
-    assert response.status_code in [404, 500]
+    # Vérifiez si le message d'erreur est correct
+    assert response.json()["detail"] == "Client ID not found"
 
-    # Vérifiez le contenu du message d'erreur
-    if response.status_code == 404:
-        assert response.json()["detail"] == "Client ID not found"
-    elif response.status_code == 500:
-        assert "Erreur interne" in response.json()["detail"]
 
 
 def test_predict_missing_client_id():
@@ -33,12 +31,13 @@ def test_predict_missing_client_id():
     assert response.status_code == 422  # Unprocessable Entity pour un identifiant manquant
 
 def test_api_handles_internal_error():
-    """Tester si l'API gère correctement les erreurs internes."""
-    # Envoyer une requête incorrecte pour forcer une erreur 500
+    """Tester si l'API gère correctement les erreurs de validation."""
+    # Envoyer une requête incorrecte pour forcer une erreur 422
     response = requests.post(f"{API_URL}/predict", json={"invalid_key": "value"})
-
-    # Vérifiez si l'API renvoie un code de statut 500
-    assert response.status_code == 500
+    
+    # Vérifiez si l'API renvoie un code de statut 422
+    assert response.status_code == 422
 
     # Vérifiez si un message d'erreur est présent
-    assert "detail" in response.json()
+    assert response.json()["detail"][0]["msg"] == "field required"
+
